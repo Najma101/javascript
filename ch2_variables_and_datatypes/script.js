0d648c36f4cfa540b06c543cf56b8636afaa022a
Najma101 <najma.aaden11@gmail.com>
2025-11-22 19:27:55 +0000

    first commit

---
+++ ch2_variables_and_datatypes/script.js
@@ -0,0 +1,13 @@
+var firstname = "Najma";
+var salary = 200;
+var isMarried = false;
+// var tax = 30;
+// var takehome = salary - tax;
+// console.log(firstname, salary, tax, takehome);
+
+// var lastname = "Aden";
+// var total = firstname * lastname;
+// console.log(total);
+console.log(typeof firstname);
+console.log(typeof salary);
+console.log(typeof isMarried);
