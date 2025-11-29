217ee3637c2ddfce7edb9664f5e70b0c9a76ab45
Najma101 <najma.aaden11@gmail.com>
2025-11-29 19:15:38 +0000

    i added content for chapter 4 functions

---
+++ ch4_functions/script.js
@@ -0,0 +1,56 @@
+function whatTheBoysLike() {
+  console.log("football");
+  console.log("coding");
+  console.log("music");
+  console.log("reading");
+  console.log("gaming");
+  console.log("science");
+  console.log("art");
+  console.log("skateboarding");
+  console.log("cooking");
+  console.log("friends");
+  console.log("movies");
+  console.log("photography");
+  console.log(".......................................................");
+}
+
+function whatTheGirlsLike() {
+  console.log("reading");
+  console.log("coding");
+  console.log("music");
+  console.log("sports");
+  console.log("fashion");
+  console.log("science");
+  console.log("gaming");
+  console.log("art");
+  console.log("cooking");
+  console.log("friendship");
+  console.log("travel");
+  console.log("robotics");
+  console.log(".......................................................");
+}
+
+function whatTheKidsLike() {
+  console.log("cartoons");
+  console.log("playing outside");
+  console.log("puzzles");
+  console.log("toys");
+  console.log("building blocks");
+  console.log("drawing");
+  console.log("storytime");
+  console.log("video games");
+  console.log("animals");
+  console.log("science experiments");
+  console.log("dress-up");
+  console.log("sports");
+  console.log(".......................................................");
+}
+
+// whatTheGirlsLike()
+whatTheKidsLike();
+// whatTheBoysLike();
+whatTheKidsLike();
+whatTheKidsLike();
+whatTheKidsLike();
+
+
