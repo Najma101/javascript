10e1223b84989c7f5de64c0f51ee805595c4115e
Najma101 <najma.aaden11@gmail.com>
2025-12-06 20:08:43 +0000

    added content for string

---
+++ ch8_strings/script.js
@@ -0,0 +1,13 @@
+// let address = "#5, london road";
+// console.log(address.toUpperCase());
+// console.log(address.toLowerCase());
+// console.log(address.replace("london", "kismaayo"));
+// console.log(address.indexOf("n"));
+// console.log(address.charAt(6))
+
+let email = "ali@gmailcom";
+if (email.indexOf("@") == -1) {
+  console.log("the @ is missing");
+} else if (email.indexOf(".") == -1) {
+  console.log("the . is missing");
+}
