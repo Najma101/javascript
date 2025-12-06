36f67b08b31e8e00ec69cf750100345d5fccd36e
Najma101 <najma.aaden11@gmail.com>
2025-12-06 18:37:10 +0000

    added content for chapter 6 var , let, const and scope

---
+++ ch6_let_var_constant_scope/script.js
@@ -0,0 +1,13 @@
+{
+  // var firstName = "najma";
+  let firstName = "najma";
+  // const firstName = "najma";
+
+  {
+    let lastName = "Aden";
+    console.log(lastName);
+  }
+
+  firstName = "ruqiya";
+  console.log(lastName);
+}
